b75e171144e1825bc3effa193317577047226ad0
Your Name <you@example.com>
2024-11-06 07:11:55 +0800

    1. remove hardware-configuration in vm 2. use linux-only in vm

--- configurations/nixos/win-vm/default.nix
+++ configurations/nixos/win-vm/default.nix
@@ -7,19 +7,11 @@ let
 in
 {
   imports = [
-    self.nixosModules.default
+    self.homeModules.linux-only
     ./configuration.nix
-    # (self + /modules/nixos/linux/gui/hyprland.nix)
-    (self + /modules/nixos/linux/gui/gnome.nix)
-    (self + /modules/nixos/linux/gui/desktopish/fonts.nix)
-    # (self + /modules/nixos/linux/gui/desktopish/steam.nix)
-    # (self + /modules/nixos/linux/gui/_1password.nix)
   ];
 
   services.openssh.enable = true;
-  services.tailscale.enable = true;
-  services.fprintd.enable = true;
-  services.syncthing = { enable = true; user = username; dataDir = "/home/${username}/Documents"; };
 
   programs.nix-ld.enable = true; # for vscode server
 
